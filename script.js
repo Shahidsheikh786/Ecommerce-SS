b5b3c7000a224d795e4ab2866a71775a795bc2c6
Shahidsheikh786 <132165646+Shahidsheikh786@users.noreply.github.com>
2023-08-24 10:08:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+ /* countdown js */
+ 
+ let daysItem = document.querySelector("#days");
+ let hoursItem = document.querySelector("#hours");
+ let minItem = document.querySelector("#min");
+ let secItem = document.querySelector("#sec");
+ 
+  let countDown = () => {
+     let futureDate = new Date("27 August 2023");
+     let currentDate = new Date() ;
+     let myDate = futureDate - currentDate ;
+     
+ let days = Math.floor( myDate / 1000 / 60 / 60 / 24 );
+ let hours = Math.floor( myDate / 1000 / 60 / 60 ) % 24;
+ let min = Math.floor( myDate / 1000 / 60  ) % 60 ;
+      
+let sec = Math.floor( myDate / 1000 ) % 60;  
+      
+      
+    daysItem.innerHTML = days ;
+    hoursItem.innerHTML = hours ;
+    minItem.innerHTML = min ;
+    secItem.innerHTML = sec ;
+    
+ }
+
+countDown()
+
+setInterval(countDown , 1000)
+
+
+
+// navbar  hide 
+
+let navBar = document.querySelectorAll('.nav-link');
+
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+
+
+ navBar.forEach(function(a) {
+ a.addEventListener("click" , function (){
+     navCollapse.classList.remove("show");
+     })
+ })
+
+
+
+
